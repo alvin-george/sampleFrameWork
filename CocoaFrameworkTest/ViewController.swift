807b60a5fde49e3a4ac3dbc4565f638fe899f947
FTS-MAC-014 <fingent@ftsmac014smini.ftsindia.in>
2016-12-06 14:26:46 +0530

    initial

--- CocoaFrameworkTest/ViewController.swift
+++ CocoaFrameworkTest/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import sampleCocoaFramework
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let frameworkObject =  openCocoaClass.init()
+        frameworkObject.samplePublicFunction()
+        print(frameworkObject.samplePublicVariable)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
---
+++ sampleCocoaFramework.framework/Headers/sampleCocoaFramework.h
@@ -0,0 +1,19 @@
+//
+//  sampleCocoaFramework.h
+//  sampleCocoaFramework
+//
+//  Created by FTS-MAC-014 on 06/12/16.
+//  Copyright © 2016 FTS-MAC-014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sampleCocoaFramework.
+FOUNDATION_EXPORT double sampleCocoaFrameworkVersionNumber;
+
+//! Project version string for sampleCocoaFramework.
+FOUNDATION_EXPORT const unsigned char sampleCocoaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sampleCocoaFramework/PublicHeader.h>
+
+
